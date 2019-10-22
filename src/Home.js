2e93c2afee27aa0f287c2e527eab0908d8952f6f
harryma <mch1997117@gmail.com>
2019-10-22 16:45:24 -0700

    _(:3」∠)_

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import background from "./videos/background.mp4";
-import { useSpring, animated } from "react-spring";
-
-import Button from "@material-ui/core/Button";
-
 import image from "./images/free_image.jpeg";
-// import whiteImage from "./images/whiteBackground.jpg";
+import ElevateAppBar from "./Appbar";
 import "./styles.scss";
 
-import ElevateAppBar from "./Appbar";
+import { useSpring, animated } from "react-spring";
+import ReactPageScroller from "react-page-scroller";
+
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   const fadeIn = useSpring({
     opacity: 1,
     from: { opacity: 0 },
     config: { duration: 1500 }
   });
 
-  const [open, setOpen] = useState(false);
+  const fade = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    config: { duration: 400 }
+  });
 
   const onClick = () => {
     setOpen(true);
@@ -26,23 +32,37 @@ export default function Home() {
     setOpen(false);
   };
 
+  const [reactPageScroller, setRPS] = useState(null);
+
   return (
     <div>
-      <animated.div style={fadeIn}>
-        <ElevateAppBar />
-        <video autoPlay loop muted className="video">
-          <source src={background} type="video/mp4" />
-        </video>
-      </animated.div>
-      <div className="whiteBackground">
-        <div className="padding-center">
-          <Button onClick={onClick}>Get image</Button>
-          <Button onClick={reset}>Reset</Button>
+      <ElevateAppBar />
+      <ReactPageScroller ref={c => setRPS(c)}>
+        <animated.div style={fadeIn}>
+          <video autoPlay loop muted className="video">
+            <source src={background} type="video/mp4" />
+          </video>
+        </animated.div>
+
+        <div className="imageBackground">
+          {/* <div className="padding-center">
+            <Button onClick={onClick}>Get image</Button>
+            <Button onClick={reset}>Reset</Button>
+          </div> */}
+          <ButtonGroup
+            color="secondary"
+            size="large"
+            aria-label="large outlined secondary button group"
+            className="button"
+          >
+            <Button onClick={onClick}>Get image</Button>
+            <Button onClick={reset}> Reset </Button>
+          </ButtonGroup>
+          <animated.div style={fade} className="image">
+            {open ? <img src={image} alt="image" /> : null}
+          </animated.div>
         </div>
-        <div className="padding-center">
-          {open ? <img src={image} alt="image" /> : null}
-        </div>
-      </div>
+      </ReactPageScroller>
     </div>
   );
 }
